21f58fb3b6011e4882f3948b92f73966b848a4dc
Ilia4 <118041365+Ilia4@users.noreply.github.com>
2025-04-05 19:25:10 +0300

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_ip = request.remote_addr  # Получаем IP-адрес пользователя
+    # Получаем реальный IP-адрес пользователя из заголовков
+    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     print(f"IP-адрес пользователя: {user_ip}")
     return f"Ваш IP-адрес: {user_ip}"
 
 if __name__ == '__main__':
-    # Используем переменную окружения PORT, чтобы приложение работало на правильном порту
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Используем переменную окружения для порта
+    app.run(host="0.0.0.0", port=port, debug=True)
